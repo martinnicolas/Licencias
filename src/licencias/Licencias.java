7ab553b5a7ebf8fcc78b2e539c4eabc5f6a6ebf8
martin <martin@martin-PC>
2015-12-11 13:55:48 -0300

    Seteo "system look and feel"

--- src/licencias/Licencias.java
+++ src/licencias/Licencias.java
@@ -5,6 +5,11 @@
  */
 package licencias;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import licencias.views.Main;
 
 /**
@@ -17,8 +22,23 @@ public class Licencias {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        new Main().setVisible(true);
+        try {            
+            /*Defino la ventana como decorable, en caso de que quiera usar alguna decoracion*/
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            /*Defino la apariencia de la aplicaci?n (ver substance 5.0)*/
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            /*Creo el modelo, y le digo que cree la Ventana Principal de la Aplicaci?n*/
+            // TODO code application logic here
+            new Main().setVisible(true);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Licencias.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Licencias.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Licencias.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Licencias.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
